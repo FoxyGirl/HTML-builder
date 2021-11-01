54c20bc81e7e175816d036a43f4d9ff325918499
Elena Pavlova <lenlho22@gmail.com>
2021-11-02 00:19:39 +0300

    feat: add file reading

---
+++ 01-read-file/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const stdout = process.stdout;
+
+const filePath = path.resolve(__dirname, 'text.txt');
+const stream = fs.createReadStream(filePath, 'utf-8');
+
+let data = '';
+
+stream.on('data', partData => data += partData);
+stream.on('end', () => stdout.write(data));
+stream.on('error', error => console.log('Error', error.message));
